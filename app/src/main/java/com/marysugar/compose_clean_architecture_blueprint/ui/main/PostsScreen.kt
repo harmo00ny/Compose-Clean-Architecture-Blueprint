eba819d6760bb8f8364b5f9f774d07b93f86909f
harmony <harm000ny1225@gmail.com>
2022-05-16 20:39:30 +0900

    Implementation of list screen

---
+++ app/src/main/java/com/marysugar/compose_clean_architecture_blueprint/ui/main/PostsScreen.kt
@@ -0,0 +1,16 @@
+package com.marysugar.compose_clean_architecture_blueprint.ui.main
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import com.marysugar.compose_clean_architecture_blueprint.data.Post
+
+@Composable
+fun PostsScreen(
+  post: List<Post>
+) {
+  SimpleText(post.toString())
+}
+@Composable
+fun SimpleText(title: String) {
+  Text(title)
+}
